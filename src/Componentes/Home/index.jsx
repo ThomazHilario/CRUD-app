ddc65f62d2853a0151928e120992041b3e9b9e83
Thomaz Alves <thomazhilario5@gmail.com>
2023-11-09 17:38:29 -0300

    pagina home estruturada e estilizada

--- src/Componentes/Home/index.jsx
+++ src/Componentes/Home/index.jsx
@@ -1,6 +1,29 @@
+import {useState} from 'react'
+import { Link } from 'react-router-dom'
+import './home.css'
+
 function Home(){
     return(
-        <p>vc esta na home</p>
+        <form className='formStyle'>
+            {/* title form */}
+            <legend>Cadastro</legend>
+
+            {/* container email */}
+            <div className='container_input'>
+                <label>Email:</label>
+                <input type="email"/>
+            </div>
+
+            {/* container Password */}
+            <div className='container_input'>
+                <label>Password:</label>
+                <input type="password"/>
+            </div>
+
+            {/* buttons */}
+            <button className='loginButton'>Login</button>
+            <p>Não tem uma conta ? <Link>Cadastre-se</Link></p>
+        </form>
     )
 }
 
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -2,11 +2,25 @@ import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import Home from '../Componentes/Home'
 
 export default function RoutePage(){
+    // Style da main
+    const mainCss = {
+        backgroundColor:'#3682B3',
+        height:'100vh',
+
+
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center'
+    }
+
+
     return(
         <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-            </Routes>
+            <main style={mainCss}>
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                </Routes>
+            </main>
         </BrowserRouter>
     )
 }
