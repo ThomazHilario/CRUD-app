fae7b312e474179512ed0c45dd587680977bd8e4
Thomaz Alves <thomazhilario5@gmail.com>
2023-11-27 10:37:09 -0300

    Componente register
    - Adicionando verificaçãao de login a página.
    - Renderização condicional.

--- src/Componentes/Register/index.jsx
+++ src/Componentes/Register/index.jsx
@@ -1,14 +1,38 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 import {toast} from 'react-toastify'
 import {Link} from 'react-router-dom'
 import {auth} from '../../Services/firebaseConnection'
-import {createUserWithEmailAndPassword} from 'firebase/auth'
+import {createUserWithEmailAndPassword, onAuthStateChanged} from 'firebase/auth'
 import './register.css'
 
 export default function Register(){
+
     // navigate
     const navigate = useNavigate()
+
+    useEffect(() => {
+        // Funcao que verifica se o usuario ja fez login
+        async function loadAuth(){
+            try {
+                await onAuthStateChanged(auth,(user) => {
+                    if(user){
+                        navigate(`/admin/${user.uid}`)
+                    } else{
+                        setCarregado(true)
+                    }
+                })
+            } catch (e) {
+                console.log(e)
+            }
+        }
+
+        // chamando a função loadAuth
+        loadAuth()
+    },[navigate])
+
+    const [carregado,setCarregado] = useState(false)
+
 
     // States - input
     const [email,setEmail] = useState('')
@@ -50,37 +74,41 @@ export default function Register(){
                 
                 case 'auth/email-already-in-use':
                     alert('Este email ja esta em uso')
-
+                    break
                 default:
-                    break;
+                    break
             }
             
         }
     }
 
-    return(
-        <main id='mainRegister'>
-            <form className='formStyle'>
-                {/* title form */}
-                <legend>Cadastro</legend>
-
-                {/* container email */}
-                <div className='container_input'>
-                    <label>Email:</label>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-                </div>
-
-                {/* container Password */}
-                <div className='container_input'>
-                    <label>Password:</label>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                </div>
-
-                {/* buttons */}
-                <button className='registerButton' onClick={registrarUsuario}>Registrar</button>
-                
-                <p>Possui uma conta ? <Link to="/" id='linkLogin'>Conectar agora</Link></p>
-            </form>
-        </main>
-    )
+    if(carregado === false){
+        return <main className='mainRegister'><h1>Carregando</h1></main>
+    } else{
+        return(
+            <main className='mainRegister'>
+                <form className='formStyle'>
+                    {/* title form */}
+                    <legend>Cadastro</legend>
+    
+                    {/* container email */}
+                    <div className='container_input'>
+                        <label>Email:</label>
+                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    </div>
+    
+                    {/* container Password */}
+                    <div className='container_input'>
+                        <label>Password:</label>
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+    
+                    {/* buttons */}
+                    <button className='registerButton' onClick={registrarUsuario}>Registrar</button>
+                    
+                    <p>Possui uma conta ? <Link to="/" id='linkLogin'>Conectar agora</Link></p>
+                </form>
+            </main>
+        )
+    }
 }
