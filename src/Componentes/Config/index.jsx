ea0c4f915b546dfc08d14fb49a9cdef442410d8a
Thomaz Alves <thomazhilario5@gmail.com>
2023-12-20 10:30:30 -0300

    Criando componente FormDetails
    - Componente FormDetails criado paraa mostrar os detalhes da conta e ter a opção de excluir a conta.

--- src/Componentes/Config/index.jsx
+++ src/Componentes/Config/index.jsx
@@ -6,15 +6,19 @@ import {auth, database} from '../../Services/firebaseConnection'
 import {doc, deleteDoc} from 'firebase/firestore'
 import {deleteUser} from 'firebase/auth'
 import Header from '../Header'
+import PropTypes from 'prop-types'
+
+
 export default function Config(){
     // id do usuario
     const {idUser} = useParams()
 
     // buscando informacoes do usuario
     useEffect(() => {
-
+        // Pegando informações na localStorage
         let data = localStorage.getItem('user') !== '' ? JSON.parse(localStorage.getItem('user')) : null
 
+        // Armazenando valor na staate user
         setUser(data)
         
     },[])
@@ -22,28 +26,10 @@ export default function Config(){
     // state - user
     const [user, setUser] = useState({})
 
-    // Deletar a conta - handleAccount
-    async function handleAccount(e){
-        try {
-            // Cancelando envio do formulario
-            e.preventDefault()
-
-            // Deletando usuario
-            deleteUser(auth.currentUser)
-
-            // Deletando o banco de dados do usuario
-            await deleteDoc(doc(database,'clientes',user.uid))
-
-            // mensagem de sucesso
-            alert('Conta deletada')
-        } catch (e) {
-            console.log(e)
-        }
-    }
-
     return (
         <main id='main_config' className='bg-slate-900 flex'>
             
+            {/* Header component */}
             <Header/>
 
             <div id='container_config'>
@@ -55,23 +41,62 @@ export default function Config(){
                 {/* Title config */}
                 <h2 className='titleConfig'>Detalhes da conta</h2>
 
-                <div id='container_delete'>
-                    <form onSubmit={handleAccount}>
-                        <div className='campos_form_config'>
-                            <label>Id:</label>
-                            <input type='text' defaultValue={user.uid} disabled/>
-                        </div>
+                {/* Formulario de detalhes da conta */}
+                <FormDetails uid={user.uid} email={user.email}/>
 
-
-                        <div className='campos_form_config'>
-                            <label>Email:</label>
-                            <input type='email' defaultValue={user.email} disabled/>
-                        </div>
-
-                        <button type='submit'>Delete my account</button>
-                    </form>
-                </div>
             </div>
         </main>
     )
+}
+
+// Component FormDetails
+function FormDetails({uid,email}){
+
+      // Deletar a conta - handleAccount
+      async function handleAccount(e){
+        try {
+            // Cancelando envio do formulario
+            e.preventDefault()
+
+            // Deletando usuario
+            deleteUser(auth.currentUser)
+
+            // Deletando o banco de dados do usuario
+            await deleteDoc(doc(database,'clientes',uid))
+
+            // mensagem de sucesso
+            alert('Conta deletada')
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    return(
+        <div id='container_delete'>
+            
+            {/* Formulario com detalhes da conta */}
+            <form onSubmit={handleAccount}>
+
+                <div className='campos_form_config'>
+                    <label>Id:</label>
+                    <input type='text' defaultValue={uid} disabled/>
+                </div>
+
+
+                <div className='campos_form_config'>
+                    <label>Email:</label>
+                    <input type='email' defaultValue={email} disabled/>
+                </div>
+
+                {/* button para excluir a conta */}
+                <button type='submit'>Delete my account</button>
+            </form>
+        </div>
+    )
+}
+
+// propTypes FormDetails
+FormDetails.propTypes = {
+    uid:PropTypes.node,
+    email:PropTypes.node
 }
