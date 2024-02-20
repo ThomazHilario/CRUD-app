093955aeced273a29425bd075d09017c035ba3a2
Thomaz Alves <thomazhilario5@gmail.com>
2024-02-20 15:35:45 -0300

    Removendo dois options do select

--- src/Pages/Admin/index.jsx
+++ src/Pages/Admin/index.jsx
@@ -24,30 +24,33 @@ export default function Admin(){
     const [seach, setSeach] = useState('')
 
     // state - selectValue
-    const [selectValue, setSelectValue] = useState('default')
+    const [selectValue, setSelectValue] = useState('Recentes')
 
     // state - filterList
     const filterList = seach !== '' ? lista.filter(item => item.nome.toLowerCase().includes(seach.toLowerCase())) : lista
 
-    const selectFilter = filterList.sort((a,b) => {
-        if(selectValue === 'Ordem alfabetica'){
-            return a.nome.localeCompare(b.nome)
-        } else if(selectValue === 'Data de Emissão'){
-            
-            // Invertendo a data
-            let dataLast = a.createdDate.split('/')
-            let datalastInvertida = dataLast[2] + '-' + dataLast[1] + '-' + dataLast[0]
+    function selectFilter(text){
 
-            // Invertendo a data
-            let dataNext = b.createdDate.split('/')
-            let dataNextInvertida = dataNext[2] + '-' + dataNext[1] + '-' + dataNext[0]
+        setSelectValue(text)
 
-            // Retornando o array com ordenação em data
-            return new Date(datalastInvertida) - new Date(dataNextInvertida)
-        }
-    })
+        filterList.sort((a,b) => {
+            if(text === 'Ordem alfabetica'){
+                return a.nome.localeCompare(b.nome)
+            } else if(text === 'Recentes'){
+                
+                // Invertendo a data
+                let dataLast = a.createdDate.split('/')
+                let datalastInvertida = dataLast[2] + '-' + dataLast[1] + '-' + dataLast[0]
 
-    console.log(selectFilter)
+                // Invertendo a data
+                let dataNext = b.createdDate.split('/')
+                let dataNextInvertida = dataNext[2] + '-' + dataNext[1] + '-' + dataNext[0]
+
+                // Retornando o array com ordenação em data
+                return new Date(dataNextInvertida) - new Date(datalastInvertida)
+            }
+        })
+    }
 
     // pegando usuarios da lista
     useEffect(() => {
@@ -341,11 +344,9 @@ export default function Admin(){
                         <Dialog.Trigger className='h-8 text-start pl-3 bg-white/100 text-black'>{selectValue}</Dialog.Trigger>
 
                             <Dialog.Content className='absolute top-32 sm:right-[7.7vw] sm:top-40 lg:top-14 lg:right-[1.25vw]'>
-                                <menu className='flex flex-col gap-1  rounded-sm bg-white h-32 w-[170px] p-2'>
-                                    <Dialog.Close className='text-black w-[9.7rem] hover:bg-black/30' onClick={(e) => setSelectValue(e.target.textContent)}>Default</Dialog.Close>
-                                    <Dialog.Close className='text-black w-[9.7rem] hover:bg-black/30' onClick={(e) => setSelectValue(e.target.textContent)}>Data de Emissão</Dialog.Close>
-                                    <Dialog.Close className='text-black w-[9.7rem] hover:bg-black/30' onClick={(e) => setSelectValue(e.target.textContent)}>Ordem alfabetica</Dialog.Close>
-                                    <Dialog.Close className='text-black w-[9.7rem] hover:bg-black/30' onClick={(e) => setSelectValue(e.target.textContent)}>Idade</Dialog.Close>
+                                <menu className='flex flex-col gap-1  rounded-sm bg-white h-30 w-[170px] p-2'>
+                                    <Dialog.Close className='text-black w-[9.7rem] hover:bg-black/30' onClick={(e) => selectFilter(e.target.textContent)}>Recentes</Dialog.Close>
+                                    <Dialog.Close className='text-black w-[9.7rem] hover:bg-black/30' onClick={(e) => selectFilter(e.target.textContent)}>Ordem alfabetica</Dialog.Close>
                                 </menu>
                             </Dialog.Content>
                     </Dialog.Root>
