eae87c3455080ddd7fc80a3dcd94b78f45f22992
Thomaz Alves <thomazhilario5@gmail.com>
2024-02-22 13:44:51 -0300

    Colocando a visualização das informações em componente.

--- src/Componentes/TableRow/index.jsx
+++ src/Componentes/TableRow/index.jsx
@@ -1,8 +1,5 @@
 import * as Dialog from '@radix-ui/react-dialog'
 
-// Icon
-import { FaAddressCard } from "react-icons/fa";
-
 // imports Firebase
 import {doc, updateDoc} from 'firebase/firestore'
 import { database } from '../../Services/firebaseConnection';
@@ -10,6 +7,9 @@ import { database } from '../../Services/firebaseConnection';
 // React e context
 import { useContext } from 'react'
 import { Context } from '../../Context';
+
+// Components
+import BoardInformation from '../BoardInformation';
 
 // Componente para exibir os tr
 export default function TableRow({ idx, CreatedDate, nomeCliente, idadeCliente, emailCliente, telefoneCliente,deleteUser, lista, setLista, idUser}){
@@ -65,27 +65,13 @@ export default function TableRow({ idx, CreatedDate, nomeCliente, idadeCliente, 
             <td data-table="Email:">{emailCliente}</td>
             <td data-table="Telefone:">{telefoneCliente}</td>
             <td className='flex justify-start items-center gap-2 md:justify-center'>
-                <Dialog.Root>
-                    <Dialog.Trigger className=' w-[40px] h-[25px] bg-slate-600 flex justify-center items-center'><FaAddressCard size={20}/></Dialog.Trigger>
-
-                    <Dialog.Portal>
-
-                        <Dialog.Overlay className='fixed inset-0 bg-black/50 transition'/>
-
-                        <Dialog.Content className='bg-slate-700 rounded-md w-[90vw] h-[35vh] absolute -translate-y-1/2 -translate-x-[55%] top-[35%] left-[55%] sm:w-[90vw] lg:w-[40vw]'>
-                            <Dialog.Close className='bg-slate-900/50 flex justify-center items-center w-10 h-10 absolute right-0 top-0'>x</Dialog.Close>
-
-                            <div className='p-7 mt-2 text-white flex flex-col gap-2'>
-                                <h2 className='text-2xl mb-2'>Informações:</h2>
-                                <p><strong>Data de registro</strong> : {CreatedDate}</p>
-                                <p><strong>Nome</strong>: {nomeCliente}</p>
-                                <p><strong>Idade</strong>: {idadeCliente} anos</p>
-                                <p><strong>Email</strong>: {emailCliente}</p>
-                                <p><strong>Telefone</strong>: {telefoneCliente}</p>
-                            </div>
-                        </Dialog.Content>
-                    </Dialog.Portal>
-                </Dialog.Root>
+                <BoardInformation 
+                CreatedDate={CreatedDate} 
+                nomeCliente={nomeCliente} 
+                idadeCliente={idadeCliente} 
+                emailCliente={emailCliente} 
+                telefoneCliente={telefoneCliente}
+                />
                 
                 <Dialog.Root>
                     <Dialog.Trigger className='btn-edit w-[75px] bg-green-500' id='editModal' onClick={addValuesInStates}>Editar</Dialog.Trigger>
