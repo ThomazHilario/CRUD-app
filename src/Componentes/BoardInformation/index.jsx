2968704ae20605ae71e5f44fe9741da9ea430232
Thomaz Alves <thomazhilario5@gmail.com>
2024-03-28 13:41:31 -0300

    style: BoardIndormation
    - Retirando tailwind e colocando css

--- src/Componentes/BoardInformation/index.jsx
+++ src/Componentes/BoardInformation/index.jsx
@@ -4,20 +4,23 @@ import * as Dialog from '@radix-ui/react-dialog'
 import { FaAddressCard, } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
 
+//css
+import './board.css'
+
 export default function BoardInformation({CreatedDate, nomeCliente, idadeCliente, emailCliente, telefoneCliente}){
     return(
         <Dialog.Root>
-            <Dialog.Trigger className=' w-[40px] h-[25px] bg-slate-600 flex justify-center items-center'><FaAddressCard size={20}/></Dialog.Trigger>
+            <Dialog.Trigger className='trigger'><FaAddressCard size={20}/></Dialog.Trigger>
 
             <Dialog.Portal>
 
-                <Dialog.Overlay className='fixed inset-0 bg-black/50 transition'/>
+                <Dialog.Overlay className='overlay'/>
 
-                <Dialog.Content className='bg-slate-700 rounded-md w-[90vw] h-[35vh] absolute -translate-y-1/2 -translate-x-[55%] top-[35%] left-[55%] sm:w-[90vw] lg:w-[40vw]'>
-                    <Dialog.Close className='bg-slate-900/50 flex justify-center items-center w-10 h-10 absolute right-0 top-0'><IoMdClose color='white'/></Dialog.Close>
+                <Dialog.Content className='content'>
+                    <Dialog.Close className='close'><IoMdClose color='white'/></Dialog.Close>
 
-                    <div className='p-7 mt-2 text-white flex flex-col gap-2'>
-                        <h2 className='text-2xl mb-2'>Informações:</h2>
+                    <div>
+                        <h2>Informações:</h2>
                         <p><strong>Data de registro</strong> : {CreatedDate}</p>
                         <p><strong>Nome</strong>: {nomeCliente}</p>
                         <p><strong>Idade</strong>: {idadeCliente} anos</p>
