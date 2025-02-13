9d247ce04f1f56e51f27ebdecc9070ecbf79211c
Thomaz Alves <thomazhilario5@gmail.com>
2025-02-12 23:17:58 -0300

    Feat: Add style error in inputs for formAddPerson component

--- src/Componentes/FormAddPerson/index.jsx
+++ src/Componentes/FormAddPerson/index.jsx
@@ -6,12 +6,20 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
+// Css
+import './form-add-person.css'
+
 // Schema
 const schema = z.object({
-    nome:z.string(),
-    idade:z.string(),
-    email:z.string(),
-    telefone:z.string()
+    nome:z.string().min(1, 'Preencha o campo corretamente'),
+    idade:z.string().min(1, 'Preencha o campo corretamente'),
+    email:z.string().regex(/^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/, 'Este email não é válido!'),
+    telefone:z.string().min(11, 'Número de telefone invalido').max(11)
+}).required({
+    email:true,
+    idade:true,
+    nome:true,
+    telefone:true
 })
 
 // react
@@ -55,27 +63,29 @@ export function FormAddPerson({ lista, setLista,id }){
         }
     }
 
+    const existErrorInInputField = (errorInput) => errorInput ? 'field__style__form__add__person field_error_validation' : 'field__style__form__add__person'
+
     return(
         <form className='modal' id='modal_cadastro' onSubmit={handleSubmit(addPerson)}>
 
             <div className='campo_nome'>
                 <label>Nome:</label>
-                <input type='text' {...register('nome')}/>
+                <input type='text' className={existErrorInInputField(errors.nome)} {...register('nome')}/>
             </div>
 
             <div className='campo_idade'>
                 <label>Idade:</label>
-                <input type='text' {...register('idade')}/>
+                <input type='text' className={existErrorInInputField(errors.idade)} {...register('idade')}/>
             </div>
 
             <div className='campo_telefone'>
                 <label>Telefone:</label>
-                <input type='tel' {...register('telefone')}/>
+                <input type='tel' className={existErrorInInputField(errors.telefone)} {...register('telefone')}/>
             </div>
 
             <div className='campo_email'>
                 <label>Email:</label>
-                <input type='email' {...register('email')}/>
+                <input type='email' className={existErrorInInputField(errors.email)} {...register('email')}/>
             </div>
 
             <div>
